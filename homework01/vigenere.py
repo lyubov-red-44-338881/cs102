084018a1c21c2548d14220de61290d4913be28d9
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2021-10-28 00:46:29 +0300

    Реализованы функции encrypt_vigenere() иdecrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,25 @@
+plaintext = ''
+keyword = ''
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword *= len(plaintext) // len(keyword) + 1
+    for i, j in enumerate(plaintext):
+        position = (ord(j) + ord(keyword[i]))
+        ciphertext += chr(position % 26 + 65)
     return ciphertext
+print(encrypt_vigenere('PYTHON', 'INFORM'))
 
 
+
+
+ciphertext = ''
+keyword = ''
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword *= len(ciphertext) // len(keyword) + 1
+    for i, j in enumerate(ciphertext):
+        position = (ord(j) - ord(keyword[i]))
+        plaintext += chr(position % 26 + 65)
     return plaintext
+print(decrypt_vigenere('XLYVFZ', 'INFORM'))
+    
