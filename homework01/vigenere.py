b3ad700654ff3c00a4f91145764fd173809d1a16
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2021-11-17 00:29:43 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,22 +1,50 @@
-plaintext = ''
-keyword = ''
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
     ciphertext = ""
-    keyword *= len(plaintext) // len(keyword) + 1
-    for i, j in enumerate(plaintext):
-        position = (ord(j) + ord(keyword[i]))
-        ciphertext += chr(position % 26 + 65)
+    for element in range(len(plaintext)):
+        letter = plaintext[element]
+        keyword = keyword.upper()
+        shift = ord(keyword[element % len(keyword)]) - 65
+        element = 0
+        if letter.islower():
+            ciphertext += chr(97 + ((ord(letter) - 97 + shift) % 26))
+        elif letter.isupper():
+            ciphertext += chr(65 + ((ord(letter) - 65 + shift) % 26))
+        else:
+            ciphertext += letter
+        element += 1
     return ciphertext
 
 
-
-
-ciphertext = ''
-keyword = ''
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
     plaintext = ""
-    keyword *= len(ciphertext) // len(keyword) + 1
-    for i, j in enumerate(ciphertext):
-        position = (ord(j) - ord(keyword[i]))
-        plaintext += chr(position % 26 + 65)
+    for element in range(len(ciphertext)):
+        letter = ciphertext[element]
+        keyword = keyword.upper()
+        shift = ord(keyword[element % len(keyword)]) - 65
+        element = 0
+        if letter.islower():
+            plaintext += chr(97 + ((ord(letter) - 97 - shift) % 26))
+        elif bukva.isupper():
+            plaintext += chr(65 + ((ord(letter) - 65 - shift) % 26))
+        else:
+            plaintext += letter
+        element += 1
     return plaintext
