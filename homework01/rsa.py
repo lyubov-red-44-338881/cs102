e994a98aa6e8b2deacd5e2647bd88aa7895ba2bd
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2021-11-18 00:44:15 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,3 +1,4 @@
+"""importing random"""
 import random
 import typing as tp
 
@@ -65,6 +66,141 @@ def multiplicative_inverse(e: int, phi: int) -> int:
 
 
 #print(multiplicative_inverse(4553, 8268))
+
+
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    """test"""
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+
+    # n = pq
+    n = p * q
+
+    # phi = (p-1)(q-1)
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    """aaaaaaa"""
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    """pylint отстой!!!"""
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return "".join(plain)
+
+
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
+"""importing random"""
+import random
+import typing as tp
+
+
+def is_prime(n_n: int) -> bool:
+    """
+    Tests to see if a number is prime.
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    if n_n == 2:
+        return True
+    elif n_n == 1:
+        return False
+    if n_n % 2 == 0:
+        return False
+    d_d = 3
+    while d_d ** 2 <= n_n and n_n % d_d != 0:
+        d_d += 2
+    return d_d ** 2 > n_n
+    # pass
+
+
+# print (is_prime(1))
+
+
+def gcd(a_a: int, b_b: int) -> int:
+    """
+    Euclid's algorithm for determining the greatest common divisor.
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while a_a != 0 and b_b != 0:
+        if a_a > b_b:
+            a_a = a_a % b_b
+        else:
+            b_b = b_b % a_a
+    return a_a + b_b
+    # pass
+
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    x, xx, y, yy = 1, 0, 0, 1
+    w = phi
+    while phi:
+        q = e // phi
+        e, phi = phi, e % phi
+        x, xx = xx, x - xx * q
+        y, yy = yy, y - yy * q
+    k = x % w
+    return k
+    # pass
+
+
+# print(multiplicative_inverse(4553, 8268))
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
