803a2c40e09215ef77b3fa3ddba2e998fcb5c85d
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-13 23:53:17 +0300

    Update maze.py

--- homework03/maze.py
+++ homework03/maze.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
-from random import choice, randint, seed
+from random import choice, randint
 from typing import List, Optional, Tuple, Union
 
 import pandas as pd
+
 
 def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
