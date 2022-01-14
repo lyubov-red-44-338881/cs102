efd5a603acb2cec1a0035282ffeda20b3ed3f1dd
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-14 15:13:42 +0300

    Update maze.py

--- homework03/maze.py
+++ homework03/maze.py
@@ -213,10 +213,10 @@ def add_path_to_grid(
     return grid
 
 
-if name == "main":
-    print(pd.DataFrame(bin_tree_maze(5, 5)))
-    GRID = bin_treemaze(15, 15)
-    print(pd.DataFrame(GRID))
-    _, PATH = solve_maze(GRID)
-    MAZE = add_path_togrid(, PATH)
-    print(pd.DataFrame(MAZE))
+if __name__ == "__main__":
+print(pd.DataFrame(bin_tree_maze(5, 5)))
+GRID = bin_tree_maze(15, 15)
+print(pd.DataFrame(GRID))
+_, PATH = solve_maze(GRID)
+MAZE = add_path_to_grid(_, PATH)
+print(pd.DataFrame(MAZE))
