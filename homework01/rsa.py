c8879fb21fdd119c9eaa8f5816157d04abd9995c
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-15 01:40:00 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -8,7 +8,7 @@ def is_prime(n: int) -> bool:
         return False
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-             return False
+            return False
     else:
         return True
 
