0f38d4b8f88b6093b1f05029d9b6f301559a3961
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-16 00:42:00 +0300

    Update maze.py

--- homework04_new/maze.py
+++ homework04_new/maze.py
@@ -202,7 +202,8 @@ def solve_maze(grid: List[List[Union[str, int]]]) -> Optional[List[Tuple[int, in
     return grid, shortest_path(grid, ending)
 
 
-def add_path_to_grid(grid: List[List[Union[str, int]]], path: Optional[Union[Tuple[int, int], List[Tuple[int, int]]]]
+def add_path_to_grid(
+    grid: List[List[Union[str, int]]], path: Optional[Union[Tuple[int, int], List[Tuple[int, int]]]]
 ) -> List[List[Union[str, int]]]:
     if path:
         for i, row in enumerate(grid):
