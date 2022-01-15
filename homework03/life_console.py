780d424c41f2610ec05ac443bd2487f9e3c29e30
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-15 22:43:26 +0300

    Update life_console.py

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -9,14 +9,24 @@ class Console(UI):
         super().__init__(life)
 
     def draw_borders(self, screen) -> None:
-        """Отобразить рамку."""
-        pass
+        screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
-        """Отобразить состояние клеток."""
-        pass
+        for i, row in enumerate(self.life.curr_generation):
+            for j, cell in enumerate(row):
+                if cell == 1:
+                    fill = "+"
+                else:
+                    fill = " "
+                screen.addch(i + 1, j + 1, fill)
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        running = True
+        while running:
+            self.draw_grid(screen)
+            self.draw_borders(screen)
+            self.life.step()
+            screen.refresh()
+            curses.napms(100)
         curses.endwin()
