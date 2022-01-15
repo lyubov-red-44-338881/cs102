3001cee557528c58b11888ca0a45c7143a8dd512
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-15 12:35:53 +0300

    nu chavo kavo pochemu ne poluchaetsa??

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,15 @@
-def encrypt_ceasar(plaintext: str, shift: int = 3) -> str:
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
     for symbol in plaintext:
         if symbol.isalpha():
@@ -11,7 +22,18 @@ def encrypt_ceasar(plaintext: str, shift: int = 3) -> str:
     return ciphertext
 
 
-def decrypt_ceasar(ciphertext: str, shift: int = 3) -> str:
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
     for symbol in ciphertext:
         if symbol.isalpha():
@@ -22,3 +44,5 @@ def decrypt_ceasar(ciphertext: str, shift: int = 3) -> str:
         else:
             plaintext += symbol
     return plaintext
+
+
