f90b42f700d6b71ca1b381b98ebec658b278de44
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-15 12:27:45 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,4 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+def encrypt_vigenere(plaintext: str, keyword: str, shift: int = 3) -> str:
     ciphertext = ""
     for symbol in plaintext:
         if symbol.isalpha():
@@ -11,7 +11,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+def decrypt_vigenere(ciphertext: str, keyword: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
     >>> decrypt_vigenere("PYTHON", "A")
