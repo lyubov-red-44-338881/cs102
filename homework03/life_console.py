bf41cd446f424b10700954d270023387161e34aa
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-01-18 03:15:35 +0300

    Update life_console.py

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -30,8 +30,7 @@ class Console(UI):
             screen.refresh()
             curses.napms(100)
         curses.endwin()
-
-       
+      
 if __name__ == "__main__":
     life = GameOfLife((24, 80), max_generations=100)
     ui = Console(life)
