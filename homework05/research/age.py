34595258496d5fd7105069c5ae34424414c3edcd
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-04-14 17:09:54 +0300

    Update age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -8,10 +8,20 @@ from vkapi.friends import get_friends
 def age_predict(user_id: int) -> tp.Optional[float]:
     """
     Наивный прогноз возраста пользователя по возрасту его друзей.
-
     Возраст считается как медиана среди возраста всех друзей пользователя
-
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    items = get_friends(user_id, fields=["bdate"]).items
+    today = dt.datetime.now()
+    year = today.year
+    age = []
+    for i in items:
+        if "bdate" in i:  # type: ignore
+            if len(i["bdate"]) >= 9:  # type: ignore
+                age.append(year - int(i["bdate"][-4:]))  # type: ignore
+    if age:
+        av = statistics.mean(age)
+    else:
+        av = None
+    return av
