294e81bb70f97878d53525c3b03dc2810b38b168
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-04-14 18:00:12 +0300

    Update config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
-    "version": "5.126",
+    "access_token": "d806c8a33647ce3d1297f7e0738c8f2963b7d14eed3f75c49990d52ca11b16670842426025451a1ea4b2a",
+    "version": "5.131",
 }
