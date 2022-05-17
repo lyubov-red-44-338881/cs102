d3834c701fc8d179b3d285462cad9d11611be988
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-05-17 21:52:39 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String # isort: skip
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, create_engine # isort: skip
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
