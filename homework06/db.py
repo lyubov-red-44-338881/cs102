29339329e643b664984b78955b99ca92c2d6fb23
Редькина Любовь <90209167+lyubov-red-44-338881@users.noreply.github.com>
2022-05-17 21:30:35 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
